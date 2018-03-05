ef8b08de72fe0f64fba533bb94f3bbf56718de60
alex <alejandro.correro@hotmail.com>
2018-03-05 17:49:57 +0100

    Final

--- app/src/main/java/com/alejandrocorrero/room/ui/main/PreferencesFragment.java
+++ app/src/main/java/com/alejandrocorrero/room/ui/main/PreferencesFragment.java
@@ -6,14 +6,16 @@ import android.preference.EditTextPreference;
 import android.preference.ListPreference;
 import android.preference.MultiSelectListPreference;
 import android.preference.Preference;
+import android.preference.PreferenceCategory;
 import android.preference.PreferenceFragment;
+import android.preference.PreferenceScreen;
 import android.support.annotation.Nullable;
 
 import com.alejandrocorrero.room.R;
 
 import java.util.Set;
 
-public class PreferencesFragment extends PreferenceFragment implements SharedPreferences.OnSharedPreferenceChangeListener{
+public class PreferencesFragment extends PreferenceFragment implements SharedPreferences.OnSharedPreferenceChangeListener {
 
 
     public PreferencesFragment() {
@@ -24,6 +26,30 @@ public class PreferencesFragment extends PreferenceFragment implements SharedPre
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.preferences);
+        initSummaries();
+    }
+
+    private void initSummaries() {
+        for (int i = 0; i < getPreferenceScreen().getPreferenceCount(); i++) {
+            initSummary(getPreferenceScreen().getPreference(i));
+        }
+    }
+
+    private void initSummary(Preference preference) {
+        if (preference instanceof PreferenceScreen) {
+            PreferenceScreen preferenceScreen = (PreferenceScreen) preference;
+            for (int i = 0; i < preferenceScreen.getPreferenceCount(); i++) {
+                initSummary(preferenceScreen.getPreference(i));
+            }
+        } else if (preference instanceof PreferenceCategory) {
+            PreferenceCategory categoria = (PreferenceCategory) preference;
+            for (int i = 0; i < categoria.getPreferenceCount(); i++) {
+                initSummary(categoria.getPreference(i));
+            }
+        } else {
+            updateSummary(preference);
+        }
+
     }
 
 
